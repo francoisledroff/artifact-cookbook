16ee154acaa7f5338ffe3f6c017e6877b40e512d
francoisledroff <francois.le.droff@gmail.com>
2014-05-02 14:12:23 +0200

    francois release

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Provides your cookbooks with the Artifact Deploy LWRP"
-version          "1.11.3"
+version          "1.11.4" #1.11.4 is my fork from 1.11.3
 
 supports "centos"
 supports "redhat"
